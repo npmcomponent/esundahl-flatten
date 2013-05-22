db9a93cb56df18f8f76ed3f2fea457ac959f072b
Erik Sundahl <esundahl@gmail.com>
2013-05-22 09:32:13 -0700

    fixes missing methods

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
-function flatten(array, isShallow, callback, thisArg) {
+/**
+ * Dependencies
+ */
+
+var isArray = require('isArray');
+
+
+module.exports = function (array, isShallow, callback, thisArg) {
   var index = -1,
       length = array ? array.length : 0,
       result = [];
@@ -9,19 +16,26 @@ function flatten(array, isShallow, callback, thisArg) {
     callback = !(thisArg && thisArg[isShallow] === array) ? isShallow : undefined;
     isShallow = false;
   }
+
   if (callback != null) {
     callback = lodash.createCallback(callback, thisArg);
   }
+
   while (++index < length) {
     var value = array[index];
+
     if (callback) {
       value = callback(value, index, array);
     }
+
     if (isArray(value)) {
-      push.apply(result, isShallow ? value : flatten(value));
-    } else {
+      Array.prototype.push.apply(result, isShallow ? value : flatten(value));
+    }
+
+    else {
       result.push(value);
     }
   }
+
   return result;
 }
